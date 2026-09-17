ab682d5d7552f7da3c12eff3620ccd32674198cc
杨盼 <yangpan@gosun-ink.com>
2026-09-17 12:39:52 +0800

    feat(web): 全局接入 Arco 语言包
    Wire Arco locale globally.
    
    - App.vue 用 a-config-provider 包裹 router-view，locale 随应用语言切换 / Wrap router-view in a-config-provider with reactive locale

--- web/default-pro/src/App.vue
+++ web/default-pro/src/App.vue
@@ -1,3 +1,14 @@
 <template>
-  <router-view />
+  <a-config-provider :locale="arcoLocale">
+    <router-view />
+  </a-config-provider>
 </template>
+
+<script setup>
+import { computed } from 'vue'
+import { useI18n } from 'vue-i18n'
+import { getArcoLocale } from '@/i18n/arco'
+
+const { locale } = useI18n()
+const arcoLocale = computed(() => getArcoLocale(locale.value))
+</script>
