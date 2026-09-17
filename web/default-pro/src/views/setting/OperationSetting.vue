74fc6372eeb4ea8a35cb80bed38df62a13c3e03d
杨盼 <yangpan@gosun-ink.com>
2026-09-17 12:40:14 +0800

    feat(web): 运营设置页多语言
    Localize operation settings page.

--- web/default-pro/src/views/setting/OperationSetting.vue
+++ web/default-pro/src/views/setting/OperationSetting.vue
@@ -2,108 +2,108 @@
   <div class="operation-page">
     <a-spin :loading="loading">
           <div class="section">
-            <h3>额度设置</h3>
+            <h3>{{ $t('settingPage.operation.quotaSection') }}</h3>
             <a-form :model="form" layout="vertical" class="setting-form">
               <a-row :gutter="[24, 8]">
-                <a-col :span="6"><a-form-item label="新用户初始额度"><a-input-number v-model="form.QuotaForNewUser" :style="{width:'100%'}" size="large" /></a-form-item></a-col>
-                <a-col :span="6"><a-form-item label="预扣额度"><a-input-number v-model="form.PreConsumedQuota" :style="{width:'100%'}" size="large" /></a-form-item></a-col>
-                <a-col :span="6"><a-form-item label="邀请人奖励额度"><a-input-number v-model="form.QuotaForInviter" :style="{width:'100%'}" size="large" /></a-form-item></a-col>
-                <a-col :span="6"><a-form-item label="被邀请人奖励额度"><a-input-number v-model="form.QuotaForInvitee" :style="{width:'100%'}" size="large" /></a-form-item></a-col>
+                <a-col :span="6"><a-form-item :label="$t('settingPage.operation.quotaForNewUser')"><a-input-number v-model="form.QuotaForNewUser" :style="{width:'100%'}" size="large" /></a-form-item></a-col>
+                <a-col :span="6"><a-form-item :label="$t('settingPage.operation.preConsumedQuota')"><a-input-number v-model="form.PreConsumedQuota" :style="{width:'100%'}" size="large" /></a-form-item></a-col>
+                <a-col :span="6"><a-form-item :label="$t('settingPage.operation.quotaForInviter')"><a-input-number v-model="form.QuotaForInviter" :style="{width:'100%'}" size="large" /></a-form-item></a-col>
+                <a-col :span="6"><a-form-item :label="$t('settingPage.operation.quotaForInvitee')"><a-input-number v-model="form.QuotaForInvitee" :style="{width:'100%'}" size="large" /></a-form-item></a-col>
               </a-row>
-              <a-form-item><a-button type="primary" @click="saveSection(['QuotaForNewUser','PreConsumedQuota','QuotaForInviter','QuotaForInvitee'])">保存额度设置</a-button></a-form-item>
+              <a-form-item><a-button type="primary" @click="saveSection(['QuotaForNewUser','PreConsumedQuota','QuotaForInviter','QuotaForInvitee'])">{{ $t('settingPage.operation.saveQuota') }}</a-button></a-form-item>
             </a-form>
           </div>
           <a-divider :margin="24" />
 
           <div class="section">
-            <h3>监控设置</h3>
+            <h3>{{ $t('settingPage.operation.monitorSection') }}</h3>
             <a-form :model="form" layout="vertical" class="setting-form">
               <a-row :gutter="[24, 8]">
-                <a-col :span="6"><a-form-item label="渠道禁用响应时间阈值(ms)"><a-input-number v-model="form.ChannelDisableThreshold" :style="{width:'100%'}" size="large" /></a-form-item></a-col>
-                <a-col :span="6"><a-form-item label="额度提醒阈值"><a-input-number v-model="form.QuotaRemindThreshold" :style="{width:'100%'}" size="large" /></a-form-item></a-col>
+                <a-col :span="6"><a-form-item :label="$t('settingPage.operation.channelDisableThreshold')"><a-input-number v-model="form.ChannelDisableThreshold" :style="{width:'100%'}" size="large" /></a-form-item></a-col>
+                <a-col :span="6"><a-form-item :label="$t('settingPage.operation.quotaRemindThreshold')"><a-input-number v-model="form.QuotaRemindThreshold" :style="{width:'100%'}" size="large" /></a-form-item></a-col>
               </a-row>
               <a-row :gutter="[32, 8]">
-                <a-col :span="6"><a-form-item label="渠道出错自动禁用"><a-switch v-model="form.AutomaticDisableChannelEnabled" @change="saveSwitch('AutomaticDisableChannelEnabled')" /></a-form-item></a-col>
-                <a-col :span="6"><a-form-item label="自动启用恢复渠道"><a-switch v-model="form.AutomaticEnableChannelEnabled" @change="saveSwitch('AutomaticEnableChannelEnabled')" /></a-form-item></a-col>
-                <a-col :span="6"><a-form-item label="启用消费日志"><a-switch v-model="form.LogConsumeEnabled" @change="saveSwitch('LogConsumeEnabled')" /></a-form-item></a-col>
+                <a-col :span="6"><a-form-item :label="$t('settingPage.operation.autoDisableChannel')"><a-switch v-model="form.AutomaticDisableChannelEnabled" @change="saveSwitch('AutomaticDisableChannelEnabled')" /></a-form-item></a-col>
+                <a-col :span="6"><a-form-item :label="$t('settingPage.operation.autoEnableChannel')"><a-switch v-model="form.AutomaticEnableChannelEnabled" @change="saveSwitch('AutomaticEnableChannelEnabled')" /></a-form-item></a-col>
+                <a-col :span="6"><a-form-item :label="$t('settingPage.operation.logConsumeEnabled')"><a-switch v-model="form.LogConsumeEnabled" @change="saveSwitch('LogConsumeEnabled')" /></a-form-item></a-col>
               </a-row>
-              <a-form-item><a-button type="primary" @click="saveSection(['ChannelDisableThreshold','QuotaRemindThreshold'])">保存监控设置</a-button></a-form-item>
+              <a-form-item><a-button type="primary" @click="saveSection(['ChannelDisableThreshold','QuotaRemindThreshold'])">{{ $t('settingPage.operation.saveMonitor') }}</a-button></a-form-item>
             </a-form>
           </div>
           <a-divider :margin="24" />
 
           <div class="section">
-            <h3>日志清理</h3>
+            <h3>{{ $t('settingPage.operation.logCleanSection') }}</h3>
             <a-form layout="vertical" class="setting-form">
               <a-row :gutter="16" align="center">
-                <a-col :span="14"><a-form-item label="清理指定日期之前的日志"><a-date-picker v-model="logCleanDate" style="width:100%" size="large" /></a-form-item></a-col>
-                <a-col style="margin-top:28px"><a-button size="large" @click="cleanLogs" :loading="logCleaning">清理日志</a-button></a-col>
+                <a-col :span="14"><a-form-item :label="$t('settingPage.operation.cleanLogsBefore')"><a-date-picker v-model="logCleanDate" style="width:100%" size="large" /></a-form-item></a-col>
+                <a-col style="margin-top:28px"><a-button size="large" @click="cleanLogs" :loading="logCleaning">{{ $t('settingPage.operation.cleanLogs') }}</a-button></a-col>
               </a-row>
             </a-form>
           </div>
           <a-divider :margin="24" />
 
           <div class="section">
-            <h3>通用运营</h3>
+            <h3>{{ $t('settingPage.operation.generalSection') }}</h3>
             <a-form :model="form" layout="vertical" class="setting-form">
               <a-row :gutter="[24, 8]">
-                <a-col :span="8"><a-form-item label="充值链接"><a-input v-model="form.TopUpLink" placeholder="充值页面URL" size="large" /></a-form-item></a-col>
-                <a-col :span="8"><a-form-item label="Chat链接"><a-input v-model="form.ChatLink" placeholder="Chat页面URL" size="large" /></a-form-item></a-col>
-                <a-col :span="4"><a-form-item label="每单位额度价格"><a-input-number v-model="form.QuotaPerUnit" :style="{width:'100%'}" :precision="2" size="large" /></a-form-item></a-col>
-                <a-col :span="4"><a-form-item label="重试次数"><a-input-number v-model="form.RetryTimes" :style="{width:'100%'}" size="large" /></a-form-item></a-col>
+                <a-col :span="8"><a-form-item :label="$t('settingPage.operation.topUpLink')"><a-input v-model="form.TopUpLink" :placeholder="$t('settingPage.operation.topUpLinkPlaceholder')" size="large" /></a-form-item></a-col>
+                <a-col :span="8"><a-form-item :label="$t('settingPage.operation.chatLink')"><a-input v-model="form.ChatLink" :placeholder="$t('settingPage.operation.chatLinkPlaceholder')" size="large" /></a-form-item></a-col>
+                <a-col :span="4"><a-form-item :label="$t('settingPage.operation.quotaPerUnit')"><a-input-number v-model="form.QuotaPerUnit" :style="{width:'100%'}" :precision="2" size="large" /></a-form-item></a-col>
+                <a-col :span="4"><a-form-item :label="$t('settingPage.operation.retryTimes')"><a-input-number v-model="form.RetryTimes" :style="{width:'100%'}" size="large" /></a-form-item></a-col>
               </a-row>
               <a-row :gutter="[32, 8]">
-                <a-col :span="6"><a-form-item label="按货币显示额度"><a-switch v-model="form.DisplayInCurrencyEnabled" @change="saveSwitch('DisplayInCurrencyEnabled')" /></a-form-item></a-col>
-                <a-col :span="6"><a-form-item label="显示Token统计"><a-switch v-model="form.DisplayTokenStatEnabled" @change="saveSwitch('DisplayTokenStatEnabled')" /></a-form-item></a-col>
-                <a-col :span="6"><a-form-item label="近似Token计数"><a-switch v-model="form.ApproximateTokenEnabled" @change="saveSwitch('ApproximateTokenEnabled')" /></a-form-item></a-col>
+                <a-col :span="6"><a-form-item :label="$t('settingPage.operation.displayInCurrency')"><a-switch v-model="form.DisplayInCurrencyEnabled" @change="saveSwitch('DisplayInCurrencyEnabled')" /></a-form-item></a-col>
+                <a-col :span="6"><a-form-item :label="$t('settingPage.operation.displayTokenStat')"><a-switch v-model="form.DisplayTokenStatEnabled" @change="saveSwitch('DisplayTokenStatEnabled')" /></a-form-item></a-col>
+                <a-col :span="6"><a-form-item :label="$t('settingPage.operation.approximateToken')"><a-switch v-model="form.ApproximateTokenEnabled" @change="saveSwitch('ApproximateTokenEnabled')" /></a-form-item></a-col>
               </a-row>
             </a-form>
           </div>
           <a-divider :margin="24" />
 
           <div class="section">
-            <h3>通道路由</h3>
+            <h3>{{ $t('settingPage.operation.channelRouteSection') }}</h3>
             <a-form :model="form" layout="vertical" class="setting-form">
               <a-row :gutter="[24, 8]">
-                <a-col :span="6"><a-form-item label="默认冷却时间(秒)"><a-input-number v-model="form.ChannelDefaultCooldownSeconds" :style="{width:'100%'}" size="large" /></a-form-item></a-col>
-                <a-col :span="6"><a-form-item label="最大冷却时间(秒)"><a-input-number v-model="form.ChannelMaxCooldownSeconds" :style="{width:'100%'}" size="large" /></a-form-item></a-col>
+                <a-col :span="6"><a-form-item :label="$t('settingPage.operation.defaultCooldown')"><a-input-number v-model="form.ChannelDefaultCooldownSeconds" :style="{width:'100%'}" size="large" /></a-form-item></a-col>
+                <a-col :span="6"><a-form-item :label="$t('settingPage.operation.maxCooldown')"><a-input-number v-model="form.ChannelMaxCooldownSeconds" :style="{width:'100%'}" size="large" /></a-form-item></a-col>
               </a-row>
               <a-row :gutter="[32, 8]">
-                <a-col :span="6"><a-form-item label="启用渠道并发限制"><a-switch v-model="form.ChannelConcurrencyEnabled" @change="saveSwitch('ChannelConcurrencyEnabled')" /></a-form-item></a-col>
-                <a-col :span="6"><a-form-item label="启用粘性会话"><a-switch v-model="form.ChannelStickySessionEnabled" @change="saveSwitch('ChannelStickySessionEnabled')" /></a-form-item></a-col>
+                <a-col :span="6"><a-form-item :label="$t('settingPage.operation.channelConcurrency')"><a-switch v-model="form.ChannelConcurrencyEnabled" @change="saveSwitch('ChannelConcurrencyEnabled')" /></a-form-item></a-col>
+                <a-col :span="6"><a-form-item :label="$t('settingPage.operation.stickySession')"><a-switch v-model="form.ChannelStickySessionEnabled" @change="saveSwitch('ChannelStickySessionEnabled')" /></a-form-item></a-col>
               </a-row>
             </a-form>
           </div>
           <a-divider :margin="24" />
 
           <div class="section">
-            <h3>错误响应策略</h3>
+            <h3>{{ $t('settingPage.operation.errorStrategySection') }}</h3>
             <a-form layout="vertical" class="setting-form">
               <a-row :gutter="[32, 8]">
-                <a-col :span="6"><a-form-item label="透传 (400/404/422)"><a-switch v-model="errorNext.passthrough" /></a-form-item></a-col>
-                <a-col :span="6"><a-form-item label="重试 (500/502/503)"><a-switch v-model="errorNext.retry" /></a-form-item></a-col>
-                <a-col :span="6"><a-form-item label="禁用渠道 (401/402/403)"><a-switch v-model="errorNext.disable" /></a-form-item></a-col>
-                <a-col :span="6"><a-form-item label="冷却+重试 (429/529)"><a-switch v-model="errorNext.cooldown" /></a-form-item></a-col>
+                <a-col :span="6"><a-form-item :label="$t('settingPage.operation.passthrough')"><a-switch v-model="errorNext.passthrough" /></a-form-item></a-col>
+                <a-col :span="6"><a-form-item :label="$t('settingPage.operation.retry')"><a-switch v-model="errorNext.retry" /></a-form-item></a-col>
+                <a-col :span="6"><a-form-item :label="$t('settingPage.operation.disableChannel')"><a-switch v-model="errorNext.disable" /></a-form-item></a-col>
+                <a-col :span="6"><a-form-item :label="$t('settingPage.operation.cooldownRetry')"><a-switch v-model="errorNext.cooldown" /></a-form-item></a-col>
               </a-row>
-              <a-form-item><a-button type="primary" @click="saveAll">保存全部运营设置</a-button></a-form-item>
+              <a-form-item><a-button type="primary" @click="saveAll">{{ $t('settingPage.operation.saveAll') }}</a-button></a-form-item>
             </a-form>
           </div>
           <a-divider :margin="24" />
 
           <div class="section">
-            <h3>套餐运营</h3>
+            <h3>{{ $t('settingPage.operation.planSection') }}</h3>
             <a-form layout="vertical" class="setting-form">
               <a-row :gutter="[24, 8]">
                 <a-col :span="8">
-                  <a-form-item label="升级模式">
+                  <a-form-item :label="$t('settingPage.operation.upgradeMode')">
                     <a-radio-group v-model="planSettings.upgrade_mode" type="button">
-                      <a-radio value="price_diff">差价升级（默认）</a-radio>
-                      <a-radio value="stack">叠加</a-radio>
+                      <a-radio value="price_diff">{{ $t('settingPage.operation.upgradePriceDiff') }}</a-radio>
+                      <a-radio value="stack">{{ $t('settingPage.operation.upgradeStack') }}</a-radio>
                     </a-radio-group>
                   </a-form-item>
                 </a-col>
               </a-row>
-              <a-form-item><a-button type="primary" @click="savePlanSettings">保存套餐运营设置</a-button></a-form-item>
+              <a-form-item><a-button type="primary" @click="savePlanSettings">{{ $t('settingPage.operation.savePlan') }}</a-button></a-form-item>
             </a-form>
           </div>
         </a-spin>
@@ -112,10 +112,13 @@
 
 <script setup>
 import { ref, reactive, onMounted } from 'vue'
+import { useI18n } from 'vue-i18n'
 import { Message } from '@arco-design/web-vue'
 import { IconPlus } from '@arco-design/web-vue/es/icon'
 import api from '@/api'
 import settingApi from '@/api/setting'
+
+const { t } = useI18n()
 
 const loading = ref(false), logCleanDate = ref(''), logCleaning = ref(false)
 const form = reactive({
@@ -143,9 +146,9 @@ async function loadPlanSettings() {
 async function savePlanSettings() {
   try {
     const res = await settingApi.putPlan({ upgrade_mode: planSettings.upgrade_mode })
-    if (res?.data?.success) Message.success('已保存')
-    else Message.error(res?.data?.message || '保存失败')
-  } catch (e) { Message.error('保存失败') }
+    if (res?.data?.success) Message.success(t('settingPage.operation.saved'))
+    else Message.error(res?.data?.message || t('settingPage.operation.saveFailed'))
+  } catch (e) { Message.error(t('settingPage.operation.saveFailed')) }
 }
 
 const opKeys = ['QuotaForNewUser','PreConsumedQuota','QuotaForInviter','QuotaForInvitee','ChannelDisableThreshold','QuotaRemindThreshold','AutomaticDisableChannelEnabled','AutomaticEnableChannelEnabled','LogConsumeEnabled','TopUpLink','ChatLink','QuotaPerUnit','RetryTimes','DisplayInCurrencyEnabled','DisplayTokenStatEnabled','ApproximateTokenEnabled','ChannelDefaultCooldownSeconds','ChannelMaxCooldownSeconds','ChannelConcurrencyEnabled','ChannelStickySessionEnabled','ErrorNext']
@@ -169,22 +172,22 @@ async function loadOps() {
   } catch(e){} finally { loading.value = false }
 }
 
-async function saveSwitch(key) { try { await api.put('/api/option/', { key, value: form[key] ? 'true' : 'false' }); Message.success('已保存') } catch(e){ Message.error('保存失败') } }
+async function saveSwitch(key) { try { await api.put('/api/option/', { key, value: form[key] ? 'true' : 'false' }); Message.success(t('settingPage.operation.saved')) } catch(e){ Message.error(t('settingPage.operation.saveFailed')) } }
 
 async function saveSection(keys) {
   for (const k of keys) { try { await api.put('/api/option/', { key: k, value: String(form[k]??'') }) } catch(e){ /* continue */ } }
-  Message.success('已保存')
+  Message.success(t('settingPage.operation.saved'))
 }
 
 async function cleanLogs() {
   if (!logCleanDate.value) return; logCleaning.value = true
-  try { const ts = Math.floor(new Date(logCleanDate.value).getTime()/1000); await api.delete(`/api/log/?target_timestamp=${ts}`); Message.success('已清理') }
-  catch(e){ Message.error('清理失败') } finally { logCleaning.value = false }
+  try { const ts = Math.floor(new Date(logCleanDate.value).getTime()/1000); await api.delete(`/api/log/?target_timestamp=${ts}`); Message.success(t('settingPage.operation.cleaned')) }
+  catch(e){ Message.error(t('settingPage.operation.cleanFailed')) } finally { logCleaning.value = false }
 }
 
 async function saveAll() {
   await saveSection(['TopUpLink','ChatLink','QuotaPerUnit','RetryTimes','ChannelDefaultCooldownSeconds','ChannelMaxCooldownSeconds'])
-  try { await api.put('/api/option/', { key: 'ErrorNext', value: JSON.stringify({...errorNext}) }); Message.success('全部已保存') } catch(e){ Message.error('保存失败') }
+  try { await api.put('/api/option/', { key: 'ErrorNext', value: JSON.stringify({...errorNext}) }); Message.success(t('settingPage.operation.allSaved')) } catch(e){ Message.error(t('settingPage.operation.saveFailed')) }
 }
 
 onMounted(() => { loadOps(); loadPlanSettings() })
