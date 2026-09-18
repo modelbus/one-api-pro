5d7dfad790f38251366c1813bc5744a43979def7
lonaking <lonaking@foxmail.com>
2026-09-18 10:57:40 +0800

    feat(docs): 新增自定义文档主题入口
    Add the custom docs theme entry.
    
    新增 docs/doc-site/.vitepress/theme/index.js
    Add docs/doc-site/.vitepress/theme/index.js

---
+++ docs/doc-site/.vitepress/theme/index.js
@@ -0,0 +1,8 @@
+// doc-site/.vitepress/theme/index.js
+// Custom VitePress theme: extends the default theme and layers a modern
+// visual style (brand gradient, rounded cards, refined typography).
+
+import DefaultTheme from 'vitepress/theme'
+import './custom.css'
+
+export default DefaultTheme
